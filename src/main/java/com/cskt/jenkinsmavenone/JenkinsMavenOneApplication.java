1f3c8754312c4e83b444a63eb7ffc4cc95335c93
zhangchaogan <1063378441@qq.com>
2022-07-01 10:22:36 +0800

    first commit

---
+++ src/main/java/com/cskt/jenkinsmavenone/JenkinsMavenOneApplication.java
@@ -0,0 +1,13 @@
+package com.cskt.jenkinsmavenone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsMavenOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsMavenOneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cskt/jenkinsmavenone/JenkinsMavenOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cskt.jenkinsmavenone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMavenOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
